af49b463d235eed73328364970638cd8d338b944
tgrrr <phil@tgrrr.com>
2019-04-12 15:17:06 +1000

    update file structure minus pdf's

---
+++ study-other/python-udemy-with-nath/demo python vs r for ash.R
@@ -0,0 +1,37 @@
+# python
+
+# 1. 
+title = 'drone racing'
+crash = false
+
+
+# 2.  
+
+if(crash)
+
+    print('crash report for ' title)
+    # code here to drive the drone back to base
+return
+
+
+
+# Function
+def create_database(something):
+
+    # conditional 
+    if(something):
+        print(something)
+    return
+
+title = 'drone racing'
+
+create_database(title)
+
+# R
+create_database() <- function(something){
+    if (something){
+        print(something)
+    }
+    return()
+}
+create_database(title)
